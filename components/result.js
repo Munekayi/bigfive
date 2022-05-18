7961ad1a072efe43fb35ad8daf8ee89e754206d1
Munekayi <61633264+Munekayi@users.noreply.github.com>
2022-05-18 12:34:14 +0200

    Initial commit

---
+++ components/result.js
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react";
+import calculateScore from "@alheimsins/bigfive-calculate-score";
+import generateResult from "@alheimsins/b5-result-text";
+
+import config from "../config";
+
+function Facet(props) {
+  const { title, text, score, scoreText } = props;
+  return (
+    <div className="mb-2">
+      <h3 className="text-xl">{title}</h3>
+      <p className="mb-1 font-semibold">
+        Score: {score} - {scoreText}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: text }}></p>
+    </div>
+  );
+}
+
+function Domain(props) {
+  const {
+    domain,
+    title,
+    shortDescription,
+    description,
+    text,
+    score,
+    facets
+  } = props;
+  return (
+    <div className="mb-5">
+      <h1 className="text-4xl">{title}</h1>
+      <p>{shortDescription}</p>
+      <p className="mb-2 font-semibold">Score: {score}</p>
+      <p dangerouslySetInnerHTML={{ __html: text }}></p>
+      <p dangerouslySetInnerHTML={{ __html: description }}></p>
+      <h2 className="text-2xl mb-2 mt-2">Facets</h2>
+      {facets.map((data) => (
+        <Facet {...data} key={`facet-${domain}-${data.facet}`} />
+      ))}
+    </div>
+  );
+}
+
+function Result({ result, setResult }) {
+  const [score, setScore] = useState();
+  const [text, setText] = useState();
+
+  const handleReset = () => {
+    setScore(false);
+    setResult(false);
+  };
+
+  useEffect(() => {
+    if (result) {
+      setScore(calculateScore(result));
+    }
+    if (score) {
+      setText(generateResult({ scores: score, lang: config.language }));
+    }
+  }, [result, score]);
+
+  if (!result) return null;
+  if (!text) return null;
+
+  return (
+    <div>
+      <div>
+        {text.map((data) => (
+          <Domain {...data} key={`domain-${data.domain}`} />
+        ))}
+      </div>
+      <div className="pt-5">
+        <div className="flex justify-end">
+          <button
+            type="button"
+            className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            onClick={handleReset}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Result;
---
+++ pages/index.js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import { getItems } from "@alheimsins/b5-johnson-120-ipip-neo-pi-r";
+
+import pkg from "../package.json";
+import config from "../config";
+import Intro from "../components/intro";
+import LoadFile from "../components/load-file";
+import Questions from "../components/questions";
+import Result from "../components/result";
+
+function HomePage() {
+  const [questions, setQuestions] = useState();
+  const [result, setResult] = useState();
+
+  function loadPreviousResult(event) {
+    event.preventDefault();
+    const reader = new window.FileReader();
+    const files = event.target.files;
+    reader.onload = () => {
+      const text = reader.result;
+      const result = JSON.parse(text);
+      setResult(result);
+    };
+    if (files.length === 1) {
+      reader.readAsText(files[0]);
+    }
+  }
+
+  useEffect(() => {
+    setQuestions(getItems(config.language, config.randomizeQuestions));
+  }, []);
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow">
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          <div className="px-4 py-4 sm:px-0">
+            <LoadFile
+              handler={loadPreviousResult}
+              buttonTitle="Load previous result"
+            />
+            <Intro result={result} />
+            <Questions
+              questions={questions}
+              result={result}
+              setResult={setResult}
+            />
+            <Result result={result} setResult={setResult} />
+          </div>
+        </div>
+      </main>
+      <footer className="flex justify-end p-2 text-gray-500">
+        {pkg.name} - {pkg.version}
+      </footer>
+    </div>
+  );
+}
+
+export default HomePage;
